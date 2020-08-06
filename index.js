c81df87f0fd169cba5ad6f84ef3dce4f4560e93a
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-08-06 15:34:13 -0400

    limit lightness range

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ class Color{
 const pickOrigColor = () => {
   let color
 
-  // pick hue (between 0 and 360), saturation and lightness (between 0 and 100)
+  // pick hue (between 0 and 360), saturation (between 0 and 100), and lightness (between 30 and 70)
   const hue = Math.floor(Math.random() * 360)
   const saturation = Math.floor(Math.random() * 100)
-  const lightness = Math.floor(Math.random() * 100)
+  const lightness = Math.floor(Math.random() * 50) + 30
 
   color = new Color(hue, saturation, lightness)
   return color
