70cc72fb923b2cbb12709eb19ab198bae1333182
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-08-06 16:06:05 -0400

    lighten and darken colors

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ class Color{
     this.hue = hue;
     this.saturation = saturation;
     this.lightness = lightness
+
+    this.step = 15;
   }
 
   getColorString(){
     return `hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`
   } 
+
+  getLighterColor(){
+    const newLightness = (this.lightness+this.step <= 99) ? this.lightness+this.step : 99
+    return new Color(this.hue, this.saturation, newLightness)
+  }
+
+  getDarkerColor(){
+    const newLightness = (this.lightness-this.step >= 1) ? this.lightness-this.step : 1
+    return new Color(this.hue, this.saturation, newLightness)
+  }
 }
 
 const pickOrigColor = () => {
@@ -24,12 +36,35 @@ const pickOrigColor = () => {
 
 // set original color
 const origColor = pickOrigColor()
+const monochromatic = [origColor.getDarkerColor().getDarkerColor(), 
+  origColor.getDarkerColor(), 
+  origColor, 
+  origColor.getLighterColor(),
+  origColor.getLighterColor().getLighterColor()]
+
+console.log(monochromatic)
 
 // update DOM
 const displaySpace = document.getElementById('colors')
+const monoSpace = document.getElementById('mono')
 
+// show original color
 let origColorSpace = document.createElement('div')
 origColorSpace.setAttribute('id', 'orig-color')
 origColorSpace.setAttribute('class', 'col-square')
 origColorSpace.style.backgroundColor = origColor.getColorString()
 displaySpace.appendChild(origColorSpace)
+
+// show monochromatic palette
+let monoColorSpace = document.createElement('ul')
+monoColorSpace.setAttribute('id', 'mono-palette')
+monoColorSpace.setAttribute('class', 'palette')
+
+monochromatic.forEach((color, i) => {
+  let colSpace = document.createElement('li')
+  colSpace.setAttribute('class', 'col-square')
+  colSpace.style.backgroundColor = color.getColorString()
+  monoColorSpace.appendChild(colSpace)
+})
+
+monoSpace.appendChild(monoColorSpace)
