3bcdbb23cc0745fe21a00dc7cac3173b79b42e45
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-08-17 19:45:02 -0400

    an obvious test works, so progress!

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import pkg from 'chai';
+const { expect } = pkg;
+
+import { Color, generateMonochrome, generateAnalogous, generateComplementary, generateTriadic, generateCompound, generateHexPalette } from './index.js'
+
+describe('test fun happy times', function() {
+  it ('should work!', function () {
+    expect(true).to.be.true;
+  })
+})
