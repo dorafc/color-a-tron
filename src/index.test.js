a009f8cad83afb02d310a7c6c94b54da6dfbf5bd
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-08-18 11:39:58 -0400

    create first test

--- src/index.test.js
+++ src/index.test.js
@@ -3,8 +3,23 @@ const { expect } = pkg;
 
 import { Color, generateMonochrome, generateAnalogous, generateComplementary, generateTriadic, generateCompound, generateHexPalette } from './index.js'
 
-describe('test fun happy times', function() {
-  it ('should work!', function () {
+// silly test to make sure that tests work 
+describe('test fun happy times', () => {
+  it ('should work!', () => {
     expect(true).to.be.true;
+  })
+})
+
+// test constructor
+describe('create a Color object', () => {
+  it('should create an object with HSL values', () => {
+    const h = 0
+    const s = 100
+    const l = 50
+
+    let col = new Color(h,s,l)
+    expect(col.hue).to.equal(0)
+    expect(col.saturation).to.equal(100)
+    expect(col.lightness).to.equal(50)
   })
 })
