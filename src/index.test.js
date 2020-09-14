a02caef186b2143e47d160919cd867a596954d93
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-09-14 12:23:25 -0400

    test(tests): remove chai, use expect from jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,6 @@
-import pkg from 'chai';
-const { expect } = pkg;
 
 import { Color, normalizeHue, normalizeSaturation, generateMonochrome, generateAnalogous, generateComplementary, generateTriadic, generateCompound, generateHexPalette } from './index.js'
+// import { expect } from 'chai'
 
 /* TEST COLOR OBJECT */
 
@@ -14,13 +13,13 @@ describe('create a Color object', () => {
 
     let col = new Color(h,s[0],l[0])
     let col2 = new Color(h,s[1],l[1])
-    expect(col.hue).to.equal(h)
-    expect(col.saturation).to.equal(s[0])
-    expect(col.lightness).to.equal(l[0])
+    expect(col.hue).toBe(h)
+    expect(col.saturation).toBe(s[0])
+    expect(col.lightness).toBe(l[0])
 
-    expect(col2.hue).to.equal(h)
-    expect(col2.saturation).to.equal(s[1] + 100)
-    expect(col2.lightness).to.equal(l[1] + 200)
+    expect(col2.hue).toBe(h)
+    expect(col2.saturation).toBe(s[1] + 100)
+    expect(col2.lightness).toBe(l[1] + 200)
   })
 })
 
@@ -37,8 +36,8 @@ describe('hue should be between 0 and 360', () => {
     })
 
     cols.forEach(col => {
-      expect(col.hue).to.be.above(-1)
-      expect(col.hue).to.be.below(360)
+      expect(col.hue).toBeGreaterThan(-1)
+      expect(col.hue).toBeLessThan(360)
     })
   })
 })
@@ -51,7 +50,7 @@ describe('get the string of the HSL color', () => {
     const l = 50
 
     let col = new Color(h,s,l)
-    expect(col.getColorString()).to.equal(`hsl(${h}, ${s}%, ${l}%)`)
+    expect(col.getColorString()).toBe(`hsl(${h}, ${s}%, ${l}%)`)
   })
 })
 
@@ -67,14 +66,14 @@ describe('return a lighter version of color', () => {
     let lighterCol15 = col.getLighterColor(step[0])
     let lighterCol50 = col.getLighterColor(step[1])
 
-    expect(lighterCol15.hue).to.equal(h)
-    expect(lighterCol15.saturation).to.equal(s)
-    expect(lighterCol15.lightness).to.equal(l+step[0])
+    expect(lighterCol15.hue).toBe(h)
+    expect(lighterCol15.saturation).toBe(s)
+    expect(lighterCol15.lightness).toBe(l+step[0])
 
     // cap lightness value at 90
-    expect(lighterCol50.hue).to.equal(h)
-    expect(lighterCol50.saturation).to.equal(s)
-    expect(lighterCol50.lightness).to.equal(90)
+    expect(lighterCol50.hue).toBe(h)
+    expect(lighterCol50.saturation).toBe(s)
+    expect(lighterCol50.lightness).toBe(90)
   })
 })
 
@@ -90,28 +89,31 @@ describe('return a darker version of color', () => {
     let darkerCol15 = col.getDarkerColor(step[0])
     let darkerCol50 = col.getDarkerColor(step[1])
 
-    expect(darkerCol15.hue).to.equal(h)
-    expect(darkerCol15.saturation).to.equal(s)
-    expect(darkerCol15.lightness).to.equal(l-step[0])
+    expect(darkerCol15.hue).toBe(h)
+    expect(darkerCol15.saturation).toBe(s)
+    expect(darkerCol15.lightness).toBe(l-step[0])
 
     // cap lightness value at 10
-    expect(darkerCol50.hue).to.equal(h)
-    expect(darkerCol50.saturation).to.equal(s)
-    expect(darkerCol50.lightness).to.equal(10)
+    expect(darkerCol50.hue).toBe(h)
+    expect(darkerCol50.saturation).toBe(s)
+    expect(darkerCol50.lightness).toBe(10)
   })
 })
 
 // test get new hue
 describe('return a new color with a new shifted hue from the current hue', () => {
   it('should return a new color object with a higher lightness value', () => {
-    const h = 0
+    const h = 1
     const s = 100
     const l = 50
-    let steps = [15, 375]
+    let steps = [15, 376]
 
     let col = new Color(h,s,l)
 
-    let newCols = steps.map(s => col.getNewHue(s))  
+    let newCols = steps.map(s => col.getNewHue(s))
+
+    expect(newCols[0].hue).toBe(16)
+    expect(newCols[1].hue).toBe(17)
   })
 })
 
@@ -124,9 +126,9 @@ describe('create a new color related to the current color', () => {
     let col = new Color(h,s,l)
     let newCol = col.shiftColor(1,-1,1)
 
-    expect(newCol.hue).to.equal(1)
-    expect(newCol.saturation).to.equal(99)
-    expect(newCol.lightness).to.equal(51)
+    expect(newCol.hue).toBe(1)
+    expect(newCol.saturation).toBe(99)
+    expect(newCol.lightness).toBe(51)
   })
 })
 
@@ -144,12 +146,12 @@ describe('convert the HSL value to a hex string', () => {
     let col5 = new Color(h[4],s,l)
     let col6 = new Color(h[5],s,l)
 
-    expect(col.toHex()).to.equal("#ff0000")
-    expect(col2.toHex()).to.equal("#d4ff00")
-    expect(col3.toHex()).to.equal("#00ff55")
-    expect(col4.toHex()).to.equal("#00d5ff")
-    expect(col5.toHex()).to.equal("#aa00ff")
-    expect(col6.toHex()).to.equal("#ff00d4")
+    expect(col.toHex()).toBe("#ff0000")
+    expect(col2.toHex()).toBe("#d4ff00")
+    expect(col3.toHex()).toBe("#00ff55")
+    expect(col4.toHex()).toBe("#00d5ff")
+    expect(col5.toHex()).toBe("#aa00ff")
+    expect(col6.toHex()).toBe("#ff00d4")
   })
 })
 
@@ -160,12 +162,12 @@ describe('create a hex palette', () => {
   it('should return an array with 5 hex value strings', () => {
 
     let pal = generateHexPalette()
-    expect(pal.length).to.equal(5)
+    expect(pal.length).toBe(5)
 
     pal.forEach(col => {
-      expect(col).to.have.lengthOf(7)
-      expect(col.slice(0,1)).to.equal('#')
-      expect(col).to.be.a('string');
+      expect(col).toHaveLength(7)
+      expect(col.slice(0,1)).toBe('#')
+      expect(typeof col).toBe('string');
     })
   })
 })
@@ -179,16 +181,16 @@ describe('create a monochrome palette', () => {
     const color = new Color(hue, sat, light)
 
     const pal = generateMonochrome(color)
-    expect(pal.length).to.equal(5)
-    expect(pal[0].lightness).to.be.greaterThan(light)
-    expect(pal[1].lightness).to.be.greaterThan(light)
-    expect(pal[2].lightness).to.equal(light)
-    expect(pal[3].lightness).to.be.lessThan(light)
-    expect(pal[4].lightness).to.be.lessThan(light)
+    expect(pal.length).toBe(5)
+    expect(pal[0].lightness).toBeGreaterThan(light)
+    expect(pal[1].lightness).toBeGreaterThan(light)
+    expect(pal[2].lightness).toBe(light)
+    expect(pal[3].lightness).toBeLessThan(light)
+    expect(pal[4].lightness).toBeLessThan(light)
 
     pal.forEach(col => {
-      expect(col.hue).to.equal(hue)
-      expect(col.saturation).to.equal(sat)
+      expect(col.hue).toBe(hue)
+      expect(col.saturation).toBe(sat)
     })
   })
 })
@@ -204,10 +206,10 @@ describe('create an analogus palette', () => {
 
     const pal = generateAnalogous(color)
 
-    expect(pal.length).to.equal(5)
+    expect(pal.length).toBe(5)
 
     pal.forEach(col => {
-      expect(col.hue).to.satisfy(h => (normalizeHue(h + 60) || normalizeHue(h - 60) || h))
+      expect([normalizeHue(hue + 60), normalizeHue(hue - 60), hue]).toContain(col.hue)
     })
   })
 })
@@ -223,7 +225,7 @@ describe('create a complementary palette', () => {
     const pal = generateComplementary(color)
 
     pal.forEach(col => {
-      expect(col.hue).to.satisfy(h => (normalizeHue(h + 180) || h))
+      expect([normalizeHue(hue + 180), hue]).toContain(col.hue)
     })
   })
 })
@@ -238,10 +240,8 @@ describe('create a triadic palette', () => {
 
     const pal = generateTriadic(color)
 
-    expect(pal.length).to.equal(5)
-
     pal.forEach(col => {
-      expect(col.hue).to.satisfy(h => (normalizeHue(h + 120) || normalizeHue(h - 120) || h))
+      expect([normalizeHue(hue + 120), normalizeHue(hue - 120), hue]).toContain(col.hue)
     })
   })
 })
@@ -255,10 +255,8 @@ describe('create a compound palette', () => {
 
     const pal = generateCompound(color)
 
-    expect(pal.length).to.equal(5)
-
     pal.forEach(col => {
-      expect(col.hue).to.satisfy(h => (normalizeHue(h + 160) || normalizeHue(h - 160) || h))
+      expect([normalizeHue(hue + 160), normalizeHue(hue - 160), hue]).toContain(col.hue)
     })
   })
 })
